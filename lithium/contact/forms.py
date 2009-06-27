567135d367acfe484547be17b28237cdc1276bc5
Kyle Fuller <inbox@kylefuller.co.uk>
2009-06-27 17:07:37 +0100

    Re-factored send_email in lithium.contact into ContactForm

--- lithium/contact/forms.py
+++ lithium/contact/forms.py
@@ -1,8 +1,19 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
+from django.core.mail import send_mail
 
 class ContactForm(forms.Form):
     subject = forms.CharField(label=_('Subject'), max_length=100)
     message = forms.CharField(label=_('Message'), widget=forms.Textarea)
     sender = forms.EmailField(label=_('Email address'), help_text=_('A valid e-mail address, please.'))
     cc_myself = forms.BooleanField(label=_('Send a copy to yourself?'), required=False)
+    
+    def save(self, recipients=[]):
+        if form.errors:
+            raise ValueError("The ContactForm could not be saved because the data didn't validate.")
+        
+        if self.cleaned_data['cc_myself']:
+            recipients.append(self.cleaned_data['sender'])
+        
+        send_mail(self.cleaned_data['subject'], self.cleaned_data['message'],
+            self.cleaned_data['sender'], recipients)
--- lithium/contact/views.py
+++ lithium/contact/views.py
@@ -1,4 +1,3 @@
-from django.core.mail import send_mail
 from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
@@ -10,20 +9,14 @@ def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            subject = form.cleaned_data['subject']
-            message = form.cleaned_data['message']
-            sender = form.cleaned_data['sender']
-            cc_myself = form.cleaned_data['cc_myself']
             recipients = []
             
             if settings.MANAGERS:
                 for manager in settings.MANAGERS:
                     recipients.append(manager[1])
             
-            if cc_myself:
-                recipients.append(sender)
+            form.save(recipients)
             
-            send_mail(subject, message, sender, recipients)
             return HttpResponseRedirect(reverse('contact_confirmation'))
     else:
         form = ContactForm()
