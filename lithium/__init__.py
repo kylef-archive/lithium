b7b7ae16ca25f1ebd76c00e95c1f56b873663d62
Kyle Fuller <inbox@kylefuller.co.uk>
2009-06-29 16:07:39 +0100

    Created a way to get the path of lithium.templates and added information about the default templates to the docs.

--- lithium/__init__.py
+++ lithium/__init__.py
@@ -3,3 +3,7 @@ __copyright__ = 'Copyright (c) 2008, Kyle Fuller'
 __licence__ = 'BSD'
 __author__ = 'Kyle Fuller <inbox@kylefuller.co.uk>'
 __URL__ = 'http://kylefuller.co.uk/project/lithium/'
+
+def templates_path():
+    from os.path import dirname, join
+    return join(dirname(__file__), 'templates')
