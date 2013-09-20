f5eb2b27c37271ec6e6b8a4418fea59c2b45b42b
Kyle Fuller <inbox@kylefuller.co.uk>
2013-09-20 21:35:02 +0100

    Bump to version 1.0.0

--- lithium/__init__.py
+++ lithium/__init__.py
@@ -1,5 +1,5 @@
-__version__ = '0.4.4'
-__copyright__ = 'Copyright (c) 2011, Kyle Fuller'
+__version__ = '1.0.0'
+__copyright__ = 'Copyright (c) 2011-2013, Kyle Fuller'
 __licence__ = 'BSD'
 __author__ = 'Kyle Fuller <inbox@kylefuller.co.uk>'
 __URL__ = 'http://github.com/kylef/lithium/'
@@ -7,3 +7,4 @@ __URL__ = 'http://github.com/kylef/lithium/'
 def templates_path():
     from os.path import dirname, join
     return join(dirname(__file__), 'templates')
+
